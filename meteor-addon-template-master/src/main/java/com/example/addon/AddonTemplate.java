c1b21ac4bcfcbf0362beea7cb497ee0b5c8e4abc
kusut6587-maker <kusut6587@gmail.com>
2026-08-18 16:56:09 +0300

    Update AddonTemplate.java

--- meteor-addon-template-master/src/main/java/com/example/addon/AddonTemplate.java
+++ meteor-addon-template-master/src/main/java/com/example/addon/AddonTemplate.java
@@ -1,1 +1,18 @@
-package com.example.addon;`n`nimport com.example.addon.modules.CustomAimbot;`nimport meteordevelopment.meteorclient.addons.MeteorAddon;`nimport meteordevelopment.meteorclient.systems.modules.Modules;`n`npublic class AddonTemplate extends MeteorAddon {`n    @Override`n    public void onInitialize() {`n        System.out.println("Custom Aimbot Eklentisi Yuklendi!");`n        Modules.get().add(new CustomAimbot());`n    }`n`n    @Override`n    public String getPackage() {`n        return "com.example.addon";`n    }`n}
+package com.example.addon;
+
+import com.example.addon.modules.CustomAimbot;
+import meteordevelopment.meteorclient.addons.MeteorAddon;
+import meteordevelopment.meteorclient.systems.modules.Modules;
+
+public class AddonTemplate extends MeteorAddon {
+    @Override
+    public void onInitialize() {
+        System.out.println("Custom Aimbot Eklentisi Yuklendi!");
+        Modules.get().add(new CustomAimbot());
+    }
+
+    @Override
+    public String getPackage() {
+        return "com.example.addon";
+    }
+}
